e743829f50b81c4419ce8ec2cbd4572829cec075
abrildn <abrudomingueznoya@gmail.com>
2025-09-06 19:59:51 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Precio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int precio, cantidad;
+            string linea;
+
+            Console.WriteLine("Ingrese el precio");
+            linea = Console.ReadLine();
+            precio = int.Parse(linea);
+
+            Console.WriteLine("Ingrese la cantidad");
+            linea = Console.ReadLine();
+            cantidad = int.Parse(linea);
+
+            int total = precio * cantidad;
+
+            Console.WriteLine("Total:");
+            Console.WriteLine(total);
+            Console.ReadKey();
+        }
+    }
+}
